2542eb1ae8e07ae682f7a6301bdc8b8b55afc0ef
The_ai <sans34678@gmail.com>
2022-12-29 13:43:11 -0500

    website running

--- websitescript.js
+++ websitescript.js
@@ -1,22 +1,39 @@
 var mysql = require('mysql');
 
+var result;
+
 var con = mysql.createConnection({
     host: "127.0.0.1",
     user: "website",
     password: "websiteuser123$"
 });
 
-
-con.connect(function(err) {
-  if (err) throw err;
-  //Select all customers and return the result object:
-    con.query("SELECT * FROM main.main", function (err, result, fields) {
+con.connect(function (err) {
     if (err) throw err;
-    console.log(result);
-  });
+    //Select all customers and return the result object:
+    Insert into main(PersonID, UserName, FavNum)
+    Values(1, 'Fred', 7)
+    con.query("SELECT * FROM main.main", function (err, result, fields) {
+        if (err) throw err;
+        console.log(result);
+    });
 });
 
 function test() {
-    document.write("AHHHHHHHHHH");
+
+    con.connect(function (err) {
+        if (err) throw err;
+        //Select all customers and return the result object:
+        con.query("SELECT * FROM main.main", function (err, result, fields) {
+            if (err) throw err;
+            console.log(err);
+        });
+    });
     
+    Insert into main(PersonID, UserName, FavNum)
+    Values(1, 'Fred', 7)
+    
+    
+
+
 }
